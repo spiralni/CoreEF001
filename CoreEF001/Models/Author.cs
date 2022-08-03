69c4f96f5803e7803a4c98d5877c17c767e47c83
Cesar Lopez <spiralni@gmail.com>
2022-08-02 20:38:06 -0600

    Add project files.

---
+++ CoreEF001/Models/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreEF001.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+
+        public string Name { get; set; } = default!;
+        public string WebUrl { get; set; } = default!;
+    }
+}
---
+++ CoreEF001/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace CoreEF001.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public DateTime PublishedOn { get; set; } = default!;
+        public int AuthorId { get; set; }
+        public Author Author { get; set; } = default!;
+    }
+}
